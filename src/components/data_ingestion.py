01eef72e5ad30afff4e89474b4a6726694cb56f4
sahila <sahila@indeed.com>
2023-11-08 20:32:23 -0600

    Added the preprocessing steps to the preprocessing.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -39,7 +39,10 @@ class DataIngestion(DataIngestionConfig):
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header=True)
             logging.info("Ingestion of the data is completed")            
             
-            return self.ingestion_config
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
         except Exception as e:
             raise CustomException(e, sys)
@@ -50,6 +53,8 @@ class DataIngestion(DataIngestionConfig):
 
 if __name__ == '__main__':
     O = DataIngestion()
-    O.initiate_data_ingestion()
+    train_data, test_data = O.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
     
     
